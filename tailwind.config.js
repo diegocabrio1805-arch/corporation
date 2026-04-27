19452a52abca90a4d3be336d07267b8f44f40c3a
Diego <diegocabrio1805@gmail.com>
2026-04-27 19:30:29 -0300

    fix: migrate Tailwind from CDN to local PostCSS build - styles now bundled offline-first

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./*.tsx",
+    "./*.ts",
+    "./components/**/*.{tsx,ts}",
+    "./hooks/**/*.{tsx,ts}",
+    "./utils/**/*.{tsx,ts}",
+    "./services/**/*.{tsx,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
